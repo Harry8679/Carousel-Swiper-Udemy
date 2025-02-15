ea19b4f09cb984e87255404f08b61ff2c2207421
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-15 18:58:27 +0100

    App 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import CustomCarousel from './components/CustomCarousel';
-import ImageGallery from './components/ImageGallery';
+// import ImageGallery from './components/ImageGallery';
 
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      {/* <CustomCarousel /> */}
-      <ImageGallery />
+      <CustomCarousel />
+      {/* <ImageGallery /> */}
     </div>
   );
 }
